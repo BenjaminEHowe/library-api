2be25565604167e8a7e59125dc522090fb318f4f
Benjamin Howe <ben@bh96.uk>
2016-03-12 22:46:33 +0000

    Adding other implementations

--- library_api/library.py
+++ library_api/library.py
@@ -1,20 +1,24 @@
 import re
 import requests
 
+class NotAuthenticatedError(Exception):
+    pass
+
 def library(url):
-    enterprise = '<span class="sdCopyrightLink"><a shape="rect" \
-                  title="SirsiDynix" \
-                  href="http://www.sirsidynix.com">\
-                  SirsiDynix</a></span> - Enterprise Version 4.3'
+    enterprise = ('<span class="sdCopyrightLink"><a shape="rect" '
+                  'title="SirsiDynix" href="http://www.sirsidynix.com">'
+                  'SirsiDynix</a></span>\n-\xa0Enterprise')
+    exlibrisprimo = 'Powered by ExLibris Primo'
+    webpacpro = 'WebPAC PRO &copy; Innovative Interfaces, Inc.'
     
     r = requests.get(url)
-    if(enterprise in r.text)
-
-    
+    if(enterprise in r.text):
+        from library_api.implementations.enterprise import library
+    if(exlibrisprimo in r.text):
+        from library_api.implementations.exlibrisprimo import library
+    elif(webpacpro in r.text):
+        from library_api.implementations.webpacpro import library
     else:
-        raise Exception
-    
-    class library(base):
-        pass
+        raise NotImplementedError
         
     return library
